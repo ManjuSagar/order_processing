8911c0a4162aeb989340da19b16e21867af0a179
manju <manju@mahaswami.com>
2016-05-30 10:00:19 +0530

    Validation added.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,13 +24,13 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params.slice(:name, :description))
-    @picture = Picture.create!(product_params.slice(:avatar))
-    @product.pictures << @picture
+    @product = Product.new(product_params.slice(:name, :description, :price))
     respond_to do |format|
       if @product.save
+        @picture = Picture.create!(product_params.slice(:avatar))
+        @product.pictures << @picture
          #@product.pictures.create!(product_params.slice(:pictures))
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -79,7 +79,7 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.require(:product).permit(:name, :description, :avatar)
+      params.require(:product).permit(:name, :description, :avatar, :price)
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   has_many :pictures, as: :imageable
   #accepts_nested_attributes_for :pictures
   has_many :line_items
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :pictures, presence: { message: "Image Cannot be blank." }
 end
